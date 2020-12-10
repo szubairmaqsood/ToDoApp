c3f6220d735a45476ae076e92d93022e9a2dc041
zubair-pc\zubair <szubair@gmail.com>
2020-12-10 22:41:30 +0500

    To do list now will be handled from material UI

--- src/ToDo.js
+++ src/ToDo.js
@@ -1,11 +1,22 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import {
+  ListItem,
+  ListItemText,
+  ListItemAvatar,
+  Avatar,
+} from "@material-ui/core";
 
 function ToDo(props) {
-    return (
-        <div>
-            <li>{props.text}</li>
-        </div>
-    )
+  return (
+    <div>
+      <ListItem>
+        <ListItemAvatar>
+          <Avatar></Avatar>
+        </ListItemAvatar>
+        <ListItemText primary="To Do" secondary={props.text} />
+      </ListItem>
+    </div>
+  );
 }
 
-export default ToDo
+export default ToDo;
