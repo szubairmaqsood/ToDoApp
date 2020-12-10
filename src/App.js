e19774a954a9ba525ef6eb1d1ec5b33546e8f231
zubair-pc\zubair <szubair@gmail.com>
2020-12-10 21:30:18 +0500

    We attahch event handler to Add toDoList button

--- src/App.js
+++ src/App.js
@@ -4,11 +4,18 @@ import './App.css';
 function App() {
   const [todos,settodos]=useState(['Takes dog out for walk','Take the rubbish out','I want to go out today']);
   const [input,setInput]=useState('');
+
+  const addToDo=(event)=>
+  {
+    //This will fire on button click
+    settodos([...todos,input]);
+    setInput('');
+  }
   return (
     <div className="App">
       <h1>Hello World!</h1>
       <input value={input} onChange={event=>setInput(event.target.value)}/>
-      <button>Add toDo List </button>
+      <button onClick={addToDo}>Add toDo List </button>
       <ul>
         {todos.map
           (todo=>
