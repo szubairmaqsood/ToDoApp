03b8b1c2ef24df0bc3233e52ae60cbaccae9fead
zubair-pc\zubair <szubair@gmail.com>
2020-12-10 22:18:16 +0500

    To do compoent is made which represent a single line in our to do list. and we have passed props to it

--- src/App.js
+++ src/App.js
@@ -1,36 +1,49 @@
-import React,{useState} from 'react';
-import { Button, FormControl,Input,InputLabel } from '@material-ui/core';
-import './App.css';
+import React, { useState } from "react";
+import { Button, FormControl, Input, InputLabel } from "@material-ui/core";
+import ToDo from "./ToDo";
+import "./App.css";
 
 function App() {
-  const [todos,settodos]=useState(['Takes dog out for walk','Take the rubbish out','I want to go out today']);
-  const [input,setInput]=useState('');
+  const [todos, settodos] = useState([
+    "Takes dog out for walk",
+    "Take the rubbish out",
+    "I want to go out today",
+  ]);
+  const [input, setInput] = useState("");
 
-  const addToDo=(event)=>
-  {
+  const addToDo = (event) => {
     //This will fire on button click
-    event.preventDefault();   // will stop refresh
-    settodos([...todos,input]);  // add data to our to do list
-    setInput('');              //set out input filed to empty
-  }
+    event.preventDefault(); // will stop refresh
+    settodos([...todos, input]); // add data to our to do list
+    setInput(""); //set out input filed to empty
+  };
   return (
     <div className="App">
       <h1>Hello World!</h1>
       <form>
-      
-      <FormControl>
-      <InputLabel >Write to do:</InputLabel>
-      <Input  value={input} onChange={event=>setInput(event.target.value)} />
-      </FormControl>
-      <Button disabled={!input}  variant="contained" color="primary" type="submit"  onClick={addToDo}>Add toDo</Button>
+        <FormControl>
+          <InputLabel>Write to do:</InputLabel>
+          <Input
+            value={input}
+            onChange={(event) => setInput(event.target.value)}
+          />
+        </FormControl>
+        <Button
+          disabled={!input}
+          variant="contained"
+          color="primary"
+          type="submit"
+          onClick={addToDo}
+        >
+          Add toDo
+        </Button>
       </form>
       <ul>
-        {todos.map
-          (todo=>
-          (
-          <li >{todo}</li>
-          ))}
-        
+        {todos.map((todo) => (
+          <li>
+            <ToDo text={todo}></ToDo>
+          </li>
+        ))}
       </ul>
     </div>
   );
---
+++ src/ToDo.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+function ToDo(props) {
+    return (
+        <div>
+            <li>{props.text}</li>
+        </div>
+    )
+}
+
+export default ToDo
