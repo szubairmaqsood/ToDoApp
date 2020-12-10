a43a4313321a598df3099737154725fc5cfcbd3f
zubair-pc\zubair <szubair@gmail.com>
2020-12-10 21:15:02 +0500

    We connected our input field to our input state variable. Here is the catch Write some thing in input field it will not not update as our inputvaraible contains an empty string.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React,{useState} from 'react';
 import './App.css';
 
 function App() {
-  const [todos,settodos]=useState(['Takes dog out for walk','Take the rubbish out']);
+  const [todos,settodos]=useState(['Takes dog out for walk','Take the rubbish out','I want to go out today']);
+  const [input,setInput]=useState('');
   return (
     <div className="App">
       <h1>Hello World!</h1>
-      <input />
+      <input value={input} />
       <button>Add toDo List </button>
       <ul>
         {todos.map
