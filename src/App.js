a4cafd488d228c1faa0db074b6524e4ceeb21150
zubair-pc\zubair <szubair@gmail.com>
2020-12-10 21:09:16 +0500

    Basic state is made for a component  Note:when rerender react state got rerender Map is introduced to loop in react

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
+import React,{useState} from 'react';
 import './App.css';
 
 function App() {
+  const [todos,settodos]=useState(['Takes dog out for walk','Take the rubbish out']);
   return (
     <div className="App">
       <h1>Hello World!</h1>
       <input />
-      <button>Add toDo List</button>
+      <button>Add toDo List </button>
       <ul>
-        <li>Takes dog out for walk</li>
-        <li>Take the rubbish out</li>
+        {todos.map
+          (todo=>
+          (
+          <li >{todo}</li>
+          ))}
+        
       </ul>
     </div>
   );
